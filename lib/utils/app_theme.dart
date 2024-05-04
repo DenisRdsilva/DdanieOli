b9776b5e0cb6c4cfbea02abc86952204161e6066
[Denis} <[den_ou@hotmail.com]>
2024-05-03 21:26:27 -0300

    Alterando fonte

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -26,7 +26,7 @@ const LinearGradient gradientColor = LinearGradient(
 const TextTheme textTheme = TextTheme(
   titleLarge: TextStyle(
       fontFamily: 'Saira',
-      fontSize: 35,
+      fontSize: 36,
       fontWeight: FontWeight.bold,
       color: primary),
   titleMedium: TextStyle(
